7d8b39a496dbb8948ba9d4f815d0b26492a6f237
funny-mud-pee <public@goforhistory.com>
2020-07-20 18:13:32 +0800

    Create DbTransTrait.php
    update

---
+++ src/DbTransTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace funnymudpee\thinkphp;
+
+use think\facade\Db;
+
+/**
+ * tp orm mysql支持事务嵌套，尽管套就好，不用过多处理
+ * Trait DbTransTrait
+ * @package oaa\traits
+ */
+
+/**
+ * tp orm mysql支持事务嵌套，尽管套就好，不用过多处理
+ * Trait DbTransTrait
+ * @package funnymudpee\thinkphp
+ */
+trait DbTransTrait
+{
+    /**
+     * 开始事务
+     * @access protected
+     * @return void
+     */
+    protected static function startDbTrans()
+    {
+        Db::startTrans();
+    }
+
+    /**
+     * 结束事务
+     * @access protected
+     * @param bool $bError [in opt]是否出错，标记出错时事务回滚
+     * @return void
+     */
+    protected static function endDbTrans(bool $bError = true)
+    {
+        if ($bError) {
+            Db::rollback();
+        } else {
+            Db::commit();
+        }
+    }
+}
