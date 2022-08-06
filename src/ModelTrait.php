3a292d46568d7337642e827fd0255dde31285cc9
funny-mud-pee <public@goforhistory.com>
2022-08-06 18:07:22 +0800

    Update ModelTrait.php

--- src/ModelTrait.php
+++ src/ModelTrait.php
@@ -669,7 +669,7 @@ trait ModelTrait
     public static function getPage(array $where = [], ?array $field = [], array $join = [], array $sort = [], int $listRows = 0, string $group = '')
     {
         $aConf = null;
-        if ($submitListRows = request()->param('list_rows', 0, 'intval')) {
+        if ($submitListRows = request()->param('list_rows', 10, 'intval')) {
             $aConf['list_rows'] = $submitListRows;
         } elseif ($listRows) {
             $aConf['list_rows'] = $listRows;
